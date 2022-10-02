dc2514a38c868e4934af034fcafb35473c1d92cc
Elliu Huang <elliuhuang@modv-ve703-00919.apn.wlan.private.upenn.edu>
2022-10-02 17:46:45 -0400

    images and js

--- app.js
+++ app.js
@@ -5,13 +5,20 @@ document.addEventListener('mousemove', (e) => {
     const mouseX = e.clientX;
     const mouseY = e.clientX;
 
-    const anchor = document.getElementById('anchor')
+    const anchor = document.getElementById('anchor');
     const rekt = anchor.getBoundingClientRect();
     const anchorX = (rekt.left + rekt.width) / 2;
     const anchorY = (rekt.top + rekt.height) / 2;
 
     const angleDeg = angle(mouseX, mouseY, anchorX, anchorY);
-     
+
+    console.log(angleDeg)
+
+    const eyes = document.querySelectorAll('.eye');
+    eyes.forEach(eye => {
+        eye.style.transform = `rotate(${90 + angleDeg}deg)`;
+    })
+
 })
 
 function angle(cx, cy, ex, ey) {
@@ -19,6 +26,4 @@ function angle(cx, cy, ex, ey) {
     const dx = ex - cx;
     const rad = Math.atan2(dy, dx); // range from (-pi, pi)
     const deg = 180 * rad / Math.PI; // convert to degrees for css
-
-
 }
