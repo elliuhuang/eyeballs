d6f994c8e05406d43870eeed17ae2730a8da7591
Elliu Huang <elliuhuang@Ellius-MacBook-Pro.local>
2022-10-08 08:18:26 -0700

    js still doesn't work

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ document.addEventListener('mousemove', (e) => {
     console.log(e)
 
     const mouseX = e.clientX;
-    const mouseY = e.clientX;
+    const mouseY = e.clientY;
 
-    const anchor = document.getElementById('anchor');
+    const anchor = document.getElementById('anchor')
     const rekt = anchor.getBoundingClientRect();
     const anchorX = (rekt.left + rekt.width) / 2;
     const anchorY = (rekt.top + rekt.height) / 2;
@@ -14,9 +14,10 @@ document.addEventListener('mousemove', (e) => {
 
     console.log(angleDeg)
 
-    const eyes = document.querySelectorAll('.eye');
+    const eyes = document.querySelectorAll('.eye')
     eyes.forEach(eye => {
         eye.style.transform = `rotate(${90 + angleDeg}deg)`;
+        anchor.style.filter = `hue-rotate(${angleDeg}deg)`;
     })
 
 })
